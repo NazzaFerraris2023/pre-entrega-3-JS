cdcf4bee1608525a2ad0b41af7fa9e76e246f598
Nazzareno Ferraris <fnazzareno@yahoo.com.ar>
2023-09-15 14:00:12 -0300

    parte 1 de 3ra pre entrega

---
+++ productos.js
@@ -0,0 +1,38 @@
+const lista_productos = [{
+    Id:1,
+    Foto:"./fotos/linea-mateAva/Mate-Ava-rosa.jpeg",
+    Nombre:"Mate Ava Rosa",
+    Precio:1500,
+},
+{
+    Id:2,
+    Foto:"fotos/linea-mateAva/Mate-Ava-Verde.jpeg",
+    Nombre:"Mate Ava Verde",
+    Precio:1700,
+},
+{
+    Id:3,
+    Foto:"./fotos/linea-mateAva/Mate-Avs-Azul.jpeg",
+    Nombre:"Mate Ava Azul",
+    Precio:1900,
+},
+{
+    Id:4,
+    Foto:"fotos/Mate-Criollo/Mate-criollo-azul.jpeg",
+    Nombre:"Mate Criollo Azul",
+    Precio:2500,
+},
+{
+    Id:5,
+    Foto:"fotos/Mate-Criollo/Mate-Criollo-blanco.jpeg",
+    Nombre:"Mate Criollo Blanco",
+    Precio:2500,
+},
+{
+    Id:6,
+    Foto:"fotos/Mate-Criollo/Mate-criollo-negro.jpeg",
+    Nombre:"Mate Criollo Negro",
+    Precio:2800,
+}
+
+]
